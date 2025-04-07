4373e6cae4a0e996216628f650ce4aef0bfb2294
Afini19 <nurafini@yahoo.com>
2025-04-07 11:41:06 +0800

    updated code

--- app_code/WebSearchMemberPointEnquiry.vb
+++ app_code/WebSearchMemberPointEnquiry.vb
@@ -17,52 +17,52 @@ Public Class WebSearchMemberPointEnquiry
     Public Function MemberName(ByVal theValue As String, Optional ByVal SearchAccuracy As Integer = 1) As String
 
         If (SearchAccuracy = 1) Then
-            Return "ml_Name like '%" + theValue.Replace("'", "''") + "%'"
+            Return "ml.ml_Name like '%" + theValue.Replace("'", "''") + "%'"
         End If
 
-        Return "ml_Name = '" + theValue.Replace("'", "''") + "'"
+        Return "ml.ml_Name = '" + theValue.Replace("'", "''") + "'"
     End Function
 
     Public Function MemberType(ByVal theValue As String, Optional ByVal SearchAccuracy As Integer = 1) As String
 
         If (SearchAccuracy = 1) Then
-            Return "ml_membertype like '%" + theValue.Replace("'", "''") + "%'"
+            Return "ml.ml_membertype like '%" + theValue.Replace("'", "''") + "%'"
         End If
 
-        Return "ml_membertype = '" + theValue.Replace("'", "''") + "'"
+        Return "ml.ml_membertype = '" + theValue.Replace("'", "''") + "'"
     End Function
 
     Public Function MemberPhoneNo(ByVal theValue As String, Optional ByVal SearchAccuracy As Integer = 1) As String
 
         If (SearchAccuracy = 1) Then
-            Return "ml_hpno like '%" + theValue.Replace("'", "''") + "%'"
+            Return "ml.ml_hpno like '%" + theValue.Replace("'", "''") + "%'"
         End If
 
-        Return "ml_hpno = '" + theValue.Replace("'", "''") + "'"
+        Return "ml.ml_hpno = '" + theValue.Replace("'", "''") + "'"
     End Function
 
     Public Function MemberEmail(ByVal theValue As String, Optional ByVal SearchAccuracy As Integer = 1) As String
 
         If (SearchAccuracy = 1) Then
-            Return "ml_email like '%" + theValue.Replace("'", "''") + "%'"
+            Return "ml.ml_email like '%" + theValue.Replace("'", "''") + "%'"
         End If
 
-        Return "ml_email = '" + theValue.Replace("'", "''") + "'"
+        Return "ml.ml_email = '" + theValue.Replace("'", "''") + "'"
     End Function
 
     Public Function DOBMonth(ByVal theMonthFrom As Integer, ByVal theMonthTo As Integer, Optional ByVal SearchAccuracy As Integer = 1, Optional ByVal OverrideOperator As String = "")
 
-        Return ("(MONTH(ml_dob) BETWEEN " + theMonthFrom.ToString() + " AND " + theMonthTo.ToString() + ")")
+        Return ("(MONTH(ml.ml_dob) BETWEEN " + theMonthFrom.ToString() + " AND " + theMonthTo.ToString() + ")")
 
     End Function
 
     Public Function JoinDate(ByVal theDatainYYYYMMDD As String, ByVal theValue As String, Optional ByVal SearchAccuracy As Integer = 1, Optional ByVal OverrideOperator As String = "")
 
         If (String.Compare(OverrideOperator.Trim(), "", False) <> 0) Then
-            Return (" datediff(d,'" + theDatainYYYYMMDD + "',ml_createdt) " + OverrideOperator + " " + theValue.Replace("'", "''")) + ""
+            Return (" datediff(d,'" + theDatainYYYYMMDD + "',ml.ml_createdt) " + OverrideOperator + " " + theValue.Replace("'", "''")) + ""
         End If
 
-        Return (" datediff(d,'" + theDatainYYYYMMDD + "',ml_createdt) = " + theValue.Replace("'", "''")) + ""
+        Return (" datediff(d,'" + theDatainYYYYMMDD + "',ml.ml_createdt) = " + theValue.Replace("'", "''")) + ""
 
     End Function
 
